4364a43d4f35bb42db4614d40eaee0f0cf519775
Chelo <carlosmarcelotorresvargas@gmail.com>
2024-01-07 15:42:09 -0400

    issue correciones

--- app/services/personalService.js
+++ app/services/personalService.js
@@ -53,7 +53,7 @@ module.exports = {
 			};
 			const result = await sequelize.query('INSERT INTO personal SET ?', personal);
 			// res.json({message: 'Personal type added'});
-			return Successful("Personal type added", []);
+			return Successful('Personal type added', []);
 		} catch (error) {
 			res.status(500);
 			res.send(error.message);
@@ -84,7 +84,7 @@ module.exports = {
 				},
 			});
 			if (!response) return NotFoundResponse(`Personal con el id: ${id} no existe. `);
-			return Successful('Operacion Exitosa', response.fromDataModel());
+			return Successful('Operacion Exitosa', response);
 		} catch (error) {
 			console.log(error);
 			return InternalServer('Error en el servidor');
@@ -94,25 +94,66 @@ module.exports = {
 	// * funcion para actualizar los datos de un item
 	async update(id, body) {
 		try {
-			const response = await personal.findOne({
-				where: {
-					id: id,
-				},
-			});
+			const {
+				id_rol,
+				apellido_paterno,
+				apellido_materno,
+				nombres,
+				telefono,
+				correo_electronico,
+				numero_de_cuenta,
+				tipo_de_cuenta,
+				banco,
+				ci,
+				carrera_o_curso,
+				pago_por_hora,
+				fecha_de_nacimiento,
+				profesion,
+				universidad,
+			} = body;
+			if (
+				id == undefined ||
+				id_rol == undefined ||
+				apellido_paterno == undefined ||
+				apellido_materno == undefined ||
+				nombres == undefined ||
+				ci == undefined ||
+				correo_electronico == undefined
+			) {
+				res.status(400).json({
+					message: 'Bad request. Please fill all field',
+				});
+			}
+			const personal = {
+				id_rol,
+				apellido_paterno,
+				apellido_materno,
+				nombres,
+				telefono,
+				correo_electronico,
+				numero_de_cuenta,
+				tipo_de_cuenta,
+				banco,
+				ci,
+				carrera_o_curso,
+				pago_por_hora,
+				fecha_de_nacimiento,
+				profesion,
+				universidad,
+			};
+			const connection = await getConnection();
+			const result = await connection.query('UPDATE personal SET ? WHERE id = ?', [
+				personal,
+				id,
+			]);
+			return Successful('Personal type added', []);
 
-			if (!response) return NotFoundResponse(`personal con el id: ${id} no existe.`);
-			console.log(response);
-
-			await personal.update(body, {
-				where: {
-					id: id,
-				},
-			});
-
-			return Successful('Registro actualizado', []);
+			// res.json({message: 'Personal type added'});
 		} catch (error) {
 			console.log(error);
 			return InternalServer('Error en el servidor');
+			// res.status(500);
+			// res.send(error.message);
 		}
 	},
 
